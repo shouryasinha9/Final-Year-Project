5e727eea24956073c310d9eb5725fe941e78b402
Shourya Sinha <49537001+shouryasinha9@users.noreply.github.com>
2021-07-05 23:03:49 +0530

    Update preprocess_v3.py

--- preprocess_v3.py
+++ preprocess_v3.py
@@ -3,9 +3,6 @@
 # Processes an image to extract the text portions. Primarily
 # used for pre-processing for performing OCR.
 
-# Based on the paper "Font and Background Color Independent Text Binarization" by
-# T Kasar, J Kumar and A G Ramakrishnan
-# http://www.m.cs.osakafu-u.ac.jp/cbdar2007/proceedings/papers/O1-1.pdf
 
 import cv2
 import numpy as np
